bfe8a2fff5020de99026df6e89174dc0ef96bbbe
Armando Brito <arebrif@gmail.com>
2021-03-11 23:26:10 -0400

    feat: integrate persistence
    add CouchBase Lite database manually

--- Argon/ViewController.swift
+++ Argon/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var persistence: PersistenceManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        persistence = PersistenceManager()
     }
 
 
